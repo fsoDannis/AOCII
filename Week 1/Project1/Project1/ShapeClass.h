8348b89dc84657fbcfb9268b64d5dbf9470f4a92
Dan <Dan@Dinkydetails.com>
2012-06-01 21:56:28 -0400

    Creating the initialization of the variables. The initial Values

--- Week 1/Project1/Project1/ShapeClass.h
+++ Week 1/Project1/Project1/ShapeClass.h
@@ -12,6 +12,8 @@
 
 // Each shape will have a name, number of sides, height, and base
 // Following Inheritance Video 
+
+//Creating the ENUM helps identify and pull the shapes out without a lot of code in other places!
 typedef enum
 {
     SHAPETYPE_RECTANGLE=0,
@@ -20,6 +22,7 @@ typedef enum
     SHAPETYPE_BLANK
 }EShapeType;
 
+//Defining the commonalities between the shapes. They will all have these particular things. 
 @interface ShapeClass : NSObject
 {
     EShapeType shapetype;
@@ -31,6 +34,7 @@ typedef enum
     int area;
 }
 
+// These are the Method calls to get the information. The actual coding is inside the m file. 
 -(int)GetNumSides;
 -(NSString *)GetName;
 -(int)GetArea; 
--- Week 1/Project1/Project1/ShapeClass.m
+++ Week 1/Project1/Project1/ShapeClass.m
@@ -10,4 +10,21 @@
 
 @implementation ShapeClass
 
+//Initial Initialization. Dummy values are entered and will be overwritten inside the individual classes. The only one that is not Dummy text is the ShapType. There are 3. 
+-(id)init
+{
+    self = [super init];
+    if(self != nil)
+        {
+        shapetype = 3;
+        shapename = @"There is no Spoon";
+        numSides = 666;
+        base= 666;
+        height = 666;
+        area = 666;
+        }
+    return self;
+};
+
+
 @end
