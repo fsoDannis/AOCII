22f737e6752c4b819a561f60e695b44e6a418a16
Dan <Dan@Dinkydetails.com>
2012-06-01 22:23:59 -0400

    Gave the rectangle some life!

--- Week 1/Project1/Project1/RectangleClass.m
+++ Week 1/Project1/Project1/RectangleClass.m
@@ -11,4 +11,17 @@
 
 @implementation RectangleClass
 
+//Giving the rectangle some life. This information will go inside the shape class do its things. Return values. Check itself inside the factory, and print to my label... Hopefully!
+-(id)init
+{
+    if (self = [super init])
+    {
+        shapetype = 2;
+        shapename = @"Rectangle";
+        base = 20;
+        height= 25;
+        numSides = 4;
+    }
+    return self;
+}
 @end
