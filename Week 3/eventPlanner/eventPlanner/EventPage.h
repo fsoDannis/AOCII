ec0c0350ea762ea282f01033706f7269b32f2317
Dan <Dan@Dinkydetails.com>
2012-06-11 13:35:57 -0400

    Created Second Page

---
+++ Week 3/eventPlanner/eventPlanner/EventPage.h
@@ -0,0 +1,13 @@
+//
+//  EventPage.h
+//  eventPlanner
+//
+//  Created by Annis Dan on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventPage : UIViewController
+
+@end
